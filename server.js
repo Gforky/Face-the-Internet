91eeff585d08043d95bbf9c4154cf6715e9f1267
John Pett <email@johnpett.com>
2015-06-18 18:40:38 +0200

    Adding some comments to progress.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 */
 
+// define the websockets express server
 app = require('express.io')();
 app.http().io();
 
@@ -63,40 +64,46 @@ app.io.route('loaded', function(req) {
 // image saving...
 app.io.route('image', function(req) {
 
-    var image = req.data;
+  // data send as base64 from client <canvas> element
+  var image = req.data;
 
-    function decodeBase64Image(dataString) {
-      var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
+  // remove unnecessary junk from string
+  function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+      response = {};
 
-      if (matches.length !== 3) {
-        return new Error('Invalid input string');
-      }
-
-      response.type = matches[1];
-      response.data = new Buffer(matches[2], 'base64');
-
-      return response;
+    if (matches.length !== 3) {
+      return new Error('Invalid input string');
     }
 
-    var imageBuffer = decodeBase64Image(image);
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
 
-    var date = new Date();
-    var time = date.getTime();
+    return response;
+  }
 
-    fs.writeFile('captures/capture' + time + '.png', imageBuffer.data, function (err) {
+  // create image buffer to write file to disk
+  var imageBuffer = decodeBase64Image(image);
 
-      if (err == null) {
+  // use time to give each string a unique ID
+  var date = new Date();
+  var time = date.getTime();
 
-        app.io.broadcast('saved');
+  // write file to disk
+  fs.writeFile('captures/capture' + time + '.png', imageBuffer.data, function (err) {
 
-      } else {
+    // if all is working, broadcast event to client, else throw error
+    if (err == null) {
 
-        console.log('Error: ', err);
+      app.io.broadcast('saved');
 
-      }
+    } else {
 
-    });
+      console.log('Error: ', err);
+
+    }
+
+  });
 
 });
 
@@ -106,40 +113,62 @@ app.io.route('image', function(req) {
 
 */
 
+// saved images as an array
 var images = fs.readdirSync('captures');
 
+// amount of saved images on disk
 var imageCount = images.length;
 
+// assume there are no images currently
 var imageCounter = 0;
 
+// create a random string to ID the slices
 function randomStringGenerator(length, chars) {
   var result = '';
   for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
   return result;
 }
 
+// get images function to iterate over the images saved to disk
 (function getImage() {
+
+    // use 'setTimeout' to get around memory issues
     setTimeout(function () {
+
+        // if there are more images than have been currently iterated through
         if (imageCount > imageCounter) {
 
+          // path to current image to be sliced
           var image = 'captures/' + images[imageCounter];
 
+          // use the size method to get the image width and height, useful for images submitted on mobile etc.
           gm(image).size(function(err, value){
 
+            // check for errors, TO DO: put this in 'if' statement
             console.log('Error: ', err);
 
+            // get current image width
             var imageWidth = value.width;
 
+            // get current image height
             var imageHeight = value.height;
 
+            // start slicing on first pixel
             var sliceCounter = 1;
 
+            //
             (function getSlices() {
+
+              // use 'setTimeout' to get around memory issues
               setTimeout(function() {
+
+                // if the image height is bigger than the current slice
                 if (imageHeight > sliceCounter) {
 
+                  // apply the random string to the slice name, time not needed here as it is in the parent image file name
                   var randomString = randomStringGenerator(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
+                  // crop
                   gm(image).crop(imageWidth, 1, sliceCounter, 0).write('slices/slice' + randomString + '.png', function (err) {
                     console.log('Error: ', err);
                     sliceCounter++;
@@ -152,13 +181,13 @@ function randomStringGenerator(length, chars) {
                   getImage();
 
                 }
-              }, 500);
+              }, 250);
             })();
 
           });
 
         }
-    }, 500);
+    }, 250);
 })();
 
 /*
