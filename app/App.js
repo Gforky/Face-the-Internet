1c4209a31552789bd3eceec84128f8758725dd06
John Pett <email@johnpett.com>
2015-11-12 15:10:33 +0100

    Remove smaller components.

--- app/App.js
+++ app/App.js
@@ -1,14 +1,12 @@
 /** @jsx React.DOM */
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Header = require('./components/Header.js');
 var PhotoBooth = require('./components/PhotoBooth.js');
 
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="wrapper">
-				<Header text="Hello world!"></Header>
 				<PhotoBooth></PhotoBooth>
 			</div>
 		);
