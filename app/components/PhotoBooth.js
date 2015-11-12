11ceb61b25462e3eaf80eaa449f1fa235291f01d
John Pett <email@johnpett.com>
2015-11-12 15:09:55 +0100

    PhotoBooth development.

--- app/components/PhotoBooth.js
+++ app/components/PhotoBooth.js
@@ -3,21 +3,54 @@ var React = require('react');
 
 var PhotoBooth = React.createClass({
 
-	componentDidMount: function() {
-        console.log('Start webcam...');
+    componentDidMount: function() {
+
+        console.log('----------------------------------');
+        console.log('[EVENT] ', 'Start webcam...');
+        console.log('----------------------------------');
+
+        // create cross-browser var to check for webcam support
+        navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+        if (navigator.getUserMedia) {
+            var hdConstraints = {
+                video: {
+                        mandatory: {
+                        minWidth: 1280,
+                        minHeight: 720
+                    }
+                }
+            };
+            navigator.getUserMedia(hdConstraints, successCallback, errorCallback);
+        }
+
+        function successCallback(stream) {
+            console.log('----------------------------------');
+            console.log('[EVENT] ', 'User has accepted webcam...');
+            console.log('----------------------------------');
+        }
+
+        function errorCallback(e) {
+            console.log('----------------------------------');
+            console.log('[ERROR] ', 'User has error webcam: ', e);
+            console.log('----------------------------------');
+        }
+
     },
 
-	render: function() {
+    render: function() {
 
-		this.width = window.outerWidth;
+        this.width = window.outerWidth;
 
-		this.height = window.outerHeight;
+        this.height = window.outerHeight;
 
-		return (
-			<canvas width={this.width} height={this.height}></canvas>
-		);
-	}
-	
+        return (
+            <div className="PhotoBooth">
+                <video width={this.width} height={this.height}></video>
+            </div>
+        );
+    }
+    
 });
-	
+    
 module.exports = PhotoBooth;
