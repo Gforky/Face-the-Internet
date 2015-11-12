fdc028ff4e4d5934bd6eba82a776abbb5f2a644d
John Pett <email@johnpett.com>
2015-11-12 12:06:38 +0100

    Removed smaller modules to make PhotoBooth module.

--- app/App.js
+++ app/App.js
@@ -1,15 +1,15 @@
 /** @jsx React.DOM */
 var React = require('react');
 var Header = require('./components/Header.js');
-var Canvas = require('./components/Canvas.js');
-var Video = require('./components/Video.js');
+var PhotoBooth = require('./components/PhotoBooth.js');
 
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="wrapper">
-				<Header text="Face the Internet"></Header>
-				<Canvas></Canvas>
+				
+				<PhotoBooth></PhotoBooth>
+
 			</div>
 		);
 	}
