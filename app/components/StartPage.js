1936b83d967f22e1fb7ddb9d35544658d5f4e4ec
John Pett <email@johnpett.com>
2015-11-13 15:42:23 +0100

    Added area for 3D model.

--- app/components/StartPage.js
+++ app/components/StartPage.js
@@ -7,6 +7,13 @@ var StartPage = React.createClass({
 
     componentWillMount: function() {
 
+        console.log('----------------------------------');
+        console.log('[EVENT] ', 'New user.');
+        console.log('----------------------------------');
+
+        var bust = ReactDOM.findDOMNode(this.refs.bust);
+        console.log(bust);
+
     },
 
     render: function() {
@@ -15,6 +22,7 @@ var StartPage = React.createClass({
             <div className="StartPage">
             	<h1>Headline Title</h1>
             	<button>Click to start</button>
+                <div ref="bust"></div>
             </div>
         );
     }
