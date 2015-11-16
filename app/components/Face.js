b36a4b7fbf71af052ed14101c6eb9511d60a6822
John Pett <email@johnpett.com>
2015-11-16 16:36:11 +0100

    Added React THREE module.

--- app/components/Face.js
+++ app/components/Face.js
@@ -1,231 +1,232 @@
 /** @jsx React.DOM */
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactTHREE = require('react-three');
 var THREE = require('three');
 var OrbitControls = require('three-orbit-controls')(THREE);
 
 var Face = React.createClass({
 
-    _onWindowResize: function() {
+    // _onWindowResize: function() {
 
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+    //     camera.aspect = window.innerWidth / window.innerHeight;
+    //     camera.updateProjectionMatrix();
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+    //     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    },
+    // },
 
-    _onTouchMove: function( event ) {
+    // _onTouchMove: function( event ) {
 
-        if( event.changedTouches ) {
-            x = event.changedTouches[ 0 ].pageX;
-            y = event.changedTouches[ 0 ].pageY;
-        } else {
-            x = event.clientX;
-            y = event.clientY;
-        }
+    //     if( event.changedTouches ) {
+    //         x = event.changedTouches[ 0 ].pageX;
+    //         y = event.changedTouches[ 0 ].pageY;
+    //     } else {
+    //         x = event.clientX;
+    //         y = event.clientY;
+    //     }
         
-        mouse.x = ( x / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( y / window.innerHeight ) * 2 + 1;
+    //     mouse.x = ( x / window.innerWidth ) * 2 - 1;
+    //     mouse.y = - ( y / window.innerHeight ) * 2 + 1;
 
-        this._checkIntersection();
+        // this._checkIntersection();
 
-    },
+    // },
 
-    _checkIntersection: function() {
+    // _checkIntersection: function() {
 
-        mouseVector.set( mouse.x, mouse.y, 1 );
-        projector.unprojectVector( mouseVector, camera );
+    //     mouseVector.set( mouse.x, mouse.y, 1 );
+    //     projector.unprojectVector( mouseVector, camera );
 
-        raycaster.set( camera.position, mouseVector.sub( camera.position ).normalize() );
+    //     raycaster.set( camera.position, mouseVector.sub( camera.position ).normalize() );
 
-        var intersects = raycaster.intersectObjects( [ mesh ] );
+    //     var intersects = raycaster.intersectObjects( [ mesh ] );
 
-        if ( intersects.length > 0 ) {
+    //     if ( intersects.length > 0 ) {
 
-            var p = intersects[ 0 ].point;
-            mouseHelper.position.copy( p );
-            intersection.point.copy( p );
-            var n = intersects[ 0 ].face.normal.clone();
-            n.multiplyScalar( 10 );
-            n.add( intersects[ 0 ].point );
-            intersection.normal.copy( intersects[ 0 ].face.normal );
-            mouseHelper.lookAt( n );
+    //         var p = intersects[ 0 ].point;
+    //         mouseHelper.position.copy( p );
+    //         intersection.point.copy( p );
+    //         var n = intersects[ 0 ].face.normal.clone();
+    //         n.multiplyScalar( 10 );
+    //         n.add( intersects[ 0 ].point );
+    //         intersection.normal.copy( intersects[ 0 ].face.normal );
+    //         mouseHelper.lookAt( n );
 
-            line.geometry.vertices[ 0 ].copy( intersection.point );
-            line.geometry.vertices[ 1 ].copy( n );
-            line.geometry.verticesNeedUpdate = true;
+    //         line.geometry.vertices[ 0 ].copy( intersection.point );
+    //         line.geometry.vertices[ 1 ].copy( n );
+    //         line.geometry.verticesNeedUpdate = true;
 
-            intersection.intersects = true;
+    //         intersection.intersects = true;
 
-        } else {
+    //     } else {
 
-            intersection.intersects = false;
+    //         intersection.intersects = false;
 
-        }
+    //     }
 
-    },
+    // },
 
-    _loadModel: function() {
+    // _loadModel: function() {
 
-        var loader = new THREE.JSONLoader();
+    //     var loader = new THREE.JSONLoader();
 
-        loader.load( 'model/LeePerrySmith.js', function( geometry ) {
+    //     loader.load( 'model/LeePerrySmith.js', function( geometry ) {
 
-            geometry.verticesNeedUpdate = true;
-            geometry.elementsNeedUpdate = true;
-            geometry.morphTargetsNeedUpdate = true;
-            geometry.uvsNeedUpdate = true;
-            geometry.normalsNeedUpdate = true;
-            geometry.colorsNeedUpdate = true;
-            geometry.tangentsNeedUpdate = true;
+    //         geometry.verticesNeedUpdate = true;
+    //         geometry.elementsNeedUpdate = true;
+    //         geometry.morphTargetsNeedUpdate = true;
+    //         geometry.uvsNeedUpdate = true;
+    //         geometry.normalsNeedUpdate = true;
+    //         geometry.colorsNeedUpdate = true;
+    //         geometry.tangentsNeedUpdate = true;
 
-            var material = new THREE.MeshPhongMaterial( {
-                map: THREE.ImageUtils.loadTexture( 'model/Map-COL.jpg' ),
-                specularMap: THREE.ImageUtils.loadTexture( 'model/Map-SPEC.jpg' ),
-                normalMap: THREE.ImageUtils.loadTexture( 'model/Map-NOR.jpg' ),
-                shininess: 10
-            } );
+    //         var material = new THREE.MeshPhongMaterial( {
+    //             map: THREE.ImageUtils.loadTexture( 'model/Map-COL.jpg' ),
+    //             specularMap: THREE.ImageUtils.loadTexture( 'model/Map-SPEC.jpg' ),
+    //             normalMap: THREE.ImageUtils.loadTexture( 'model/Map-NOR.jpg' ),
+    //             shininess: 10
+    //         } );
 
-            mesh = new THREE.Mesh( geometry, material );
-            scene.add( mesh );
-            mesh.scale.set( 10, 10, 10 );
+    //         mesh = new THREE.Mesh( geometry, material );
+    //         scene.add( mesh );
+    //         mesh.scale.set( 10, 10, 10 );
 
-        } );
+    //     } );
 
-    },
+    // },
 
-    componentDidMount: function() {
+    // componentDidMount: function() {
 
-        var renderer, scene, helperScene, camera, fov = 45;
-        var mesh, decal;
-        var projector, raycaster;
-        var line;
-        var spotLight, pointLight, ambientLight;
+    //     var renderer, scene, helperScene, camera, fov = 45;
+    //     var mesh, decal;
+    //     var projector, raycaster;
+    //     var line;
+    //     var spotLight, pointLight, ambientLight;
 
-        var intersection = {
-            intersects: false,
-            point: new THREE.Vector3(),
-            normal: new THREE.Vector3()
-        };
-        var controls, renderHelpers = false;
-        var mouseVector = new THREE.Vector3();
-        var mouse = new THREE.Vector2();
+    //     var intersection = {
+    //         intersects: false,
+    //         point: new THREE.Vector3(),
+    //         normal: new THREE.Vector3()
+    //     };
+    //     var controls, renderHelpers = false;
+    //     var mouseVector = new THREE.Vector3();
+    //     var mouse = new THREE.Vector2();
 
-        var decalMaterial = new THREE.MeshPhongMaterial( { 
-            specular: 0xffffff,
-            shininess: 10,
-            map: THREE.ImageUtils.loadTexture( 'model/splatter.png' ), 
-            normalMap: THREE.ImageUtils.loadTexture( 'model/wrinkle-normal.jpg' ),
-            normalScale: new THREE.Vector2( .15, .15 ),
-            transparent: true, 
-            depthTest: true, 
-            depthWrite: false, 
-            polygonOffset: true,
-            polygonOffsetFactor: -4, 
-            wireframe: false 
-        });
+    //     var decalMaterial = new THREE.MeshPhongMaterial( { 
+    //         specular: 0xffffff,
+    //         shininess: 10,
+    //         map: THREE.ImageUtils.loadTexture( 'model/splatter.png' ), 
+    //         normalMap: THREE.ImageUtils.loadTexture( 'model/wrinkle-normal.jpg' ),
+    //         normalScale: new THREE.Vector2( .15, .15 ),
+    //         transparent: true, 
+    //         depthTest: true, 
+    //         depthWrite: false, 
+    //         polygonOffset: true,
+    //         polygonOffsetFactor: -4, 
+    //         wireframe: false 
+    //     });
 
-        var decals = [];
-        var decalHelper, mouseHelper;
-        var p = new THREE.Vector3( 0, 0, 0 );
-        var r = new THREE.Vector3( 0, 0, 0 );
-        var s = new THREE.Vector3( 10, 10, 10 );
-        var up = new THREE.Vector3( 0, 1, 0 );
-        var check = new THREE.Vector3( 1, 1, 1 );
+    //     var decals = [];
+    //     var decalHelper, mouseHelper;
+    //     var p = new THREE.Vector3( 0, 0, 0 );
+    //     var r = new THREE.Vector3( 0, 0, 0 );
+    //     var s = new THREE.Vector3( 10, 10, 10 );
+    //     var up = new THREE.Vector3( 0, 1, 0 );
+    //     var check = new THREE.Vector3( 1, 1, 1 );
 
-        var params = {
-            projection: 'normal',
-            minScale: 10,
-            maxScale: 30,
-            rotate: true,
-            clear: function() {
-                removeDecals();
-            }
-        };
+    //     var params = {
+    //         projection: 'normal',
+    //         minScale: 10,
+    //         maxScale: 30,
+    //         rotate: true,
+    //         clear: function() {
+    //             removeDecals();
+    //         }
+    //     };
 
-        // Add WebGL canvas to window
-        renderer = new THREE.WebGLRenderer( { antialias: true });
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        this.container.appendChild( renderer.domElement );
+    //     // Add WebGL canvas to window
+    //     renderer = new THREE.WebGLRenderer( { antialias: true });
+    //     renderer.setSize( window.innerWidth, window.innerHeight );
+    //     this.container.appendChild( renderer.domElement );
 
-        // Set-up WebGL scene
-        scene = new THREE.Scene();
-        helperScene = new THREE.Scene();
+    //     // Set-up WebGL scene
+    //     scene = new THREE.Scene();
+    //     helperScene = new THREE.Scene();
 
-        // Set-up WebGL camera
-        camera = new THREE.PerspectiveCamera( fov, window.innerWidth / window.innerHeight, 1, 1000 );
-        camera.position.z = 100;
-        camera.target = new THREE.Vector3();
-        controls = new OrbitControls( camera, renderer.domElement );
+    //     // Set-up WebGL camera
+    //     camera = new THREE.PerspectiveCamera( fov, window.innerWidth / window.innerHeight, 1, 1000 );
+    //     camera.position.z = 100;
+    //     camera.target = new THREE.Vector3();
+    //     controls = new OrbitControls( camera, renderer.domElement );
         
-        scene.add( camera );
+    //     scene.add( camera );
 
-        // Set-up lighting
-        ambientLight = new THREE.AmbientLight( 0x111111 );
-        scene.add( ambientLight );
+    //     // Set-up lighting
+    //     ambientLight = new THREE.AmbientLight( 0x111111 );
+    //     scene.add( ambientLight );
 
-        pointLight = new THREE.PointLight( 0xff0000 );
-        pointLight.position.z = 10000;
-        pointLight.distance = 4000;
-        scene.add( pointLight );
+    //     pointLight = new THREE.PointLight( 0xff0000 );
+    //     pointLight.position.z = 10000;
+    //     pointLight.distance = 4000;
+    //     scene.add( pointLight );
 
-        pointLight2 = new THREE.PointLight( 0xff5500 );
-        pointLight2.position.z = 1000;
-        pointLight2.distance = 2000;
-        scene.add( pointLight2 );
+    //     pointLight2 = new THREE.PointLight( 0xff5500 );
+    //     pointLight2.position.z = 1000;
+    //     pointLight2.distance = 2000;
+    //     scene.add( pointLight2 );
 
-        pointLight3 = new THREE.PointLight( 0x0000ff );
-        pointLight3.position.x = -1000;
-        pointLight3.position.z = 1000;
-        pointLight3.distance = 2000;
-        scene.add( pointLight3 );
+    //     pointLight3 = new THREE.PointLight( 0x0000ff );
+    //     pointLight3.position.x = -1000;
+    //     pointLight3.position.z = 1000;
+    //     pointLight3.distance = 2000;
+    //     scene.add( pointLight3 );
 
-        spotLight = new THREE.SpotLight( 0xaaaaaa );
-        spotLight.position.set( 100, 50, 100 );
-        scene.add( spotLight );
+    //     spotLight = new THREE.SpotLight( 0xaaaaaa );
+    //     spotLight.position.set( 100, 50, 100 );
+    //     scene.add( spotLight );
 
-        // Not sure what this does yet! TO DO: ...
-        line = new THREE.Line( new THREE.Geometry( ), new THREE.LineBasicMaterial( { linewidth: 4 }) );
-        line.geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
-        line.geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
-        scene.add( line );
+    //     // Not sure what this does yet! TO DO: ...
+    //     line = new THREE.Line( new THREE.Geometry( ), new THREE.LineBasicMaterial( { linewidth: 4 }) );
+    //     line.geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+    //     line.geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+    //     scene.add( line );
 
-        this._loadModel();
+    //     this._loadModel();
 
-        projector = new THREE.Projector();
-        raycaster = new THREE.Raycaster();
+    //     projector = new THREE.Projector();
+    //     raycaster = new THREE.Raycaster();
 
-        mouseHelper = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 10 ), new THREE.MeshNormalMaterial() );
-        scene.add( mouseHelper );
-        mouseHelper.visible = false;
+    //     mouseHelper = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 10 ), new THREE.MeshNormalMaterial() );
+    //     scene.add( mouseHelper );
+    //     mouseHelper.visible = false;
 
-        window.addEventListener( 'resize', this._onWindowResize, false );
+    //     window.addEventListener( 'resize', this._onWindowResize, false );
 
-        var moved = false;
+    //     var moved = false;
 
-        controls.addEventListener( 'change', function() {
-            moved = true;
-        } );
+    //     controls.addEventListener( 'change', function() {
+    //         moved = true;
+    //     } );
 
-        controls.addEventListener( 'start', function() {
-            moved = false;
-        } );
+    //     controls.addEventListener( 'start', function() {
+    //         moved = false;
+    //     } );
 
-        controls.addEventListener( 'end', function() {
-            this._checkIntersection();
-            if( !moved ) shoot();
-        } );
+    //     controls.addEventListener( 'end', function() {
+    //         // this._checkIntersection();
+    //         if( !moved ) shoot();
+    //     } );
 
-        window.addEventListener( 'mousemove', this._onTouchMove );
-        window.addEventListener( 'touchmove', this._onTouchMove );
+    //     // window.addEventListener( 'mousemove', this._onTouchMove );
+    //     // window.addEventListener( 'touchmove', this._onTouchMove );
 
-    },
+    // },
 
-    componentWillUnmount: function() {
-        window.removeEventListener('mousemove', this._onTouchMove);
-        window.removeEventListener('touchmove', this._onTouchMove);
-    },
+    // componentWillUnmount: function() {
+    //     // window.removeEventListener('mousemove', this._onTouchMove);
+    //     // window.removeEventListener('touchmove', this._onTouchMove);
+    // },
 
     render: function() {
         return (
