abdf80172fc366de931f882b1af43f1415683233
John Pett <email@johnpett.com>
2015-11-30 18:52:40 +0100

    Commit to merge.

--- app/main.js
+++ app/main.js
@@ -11,8 +11,8 @@ var PhotoBooth = require('./components/PhotoBooth.js');
 ReactDOM.render(<App/>, document.getElementById('app'));
 
 ReactDOM.render((
-  <Router>
-    <Route path="/" component={StartPage} />
-    <Route path="/photo-booth" component={PhotoBooth} />
-  </Router>
+	<Router>
+		<Route path="/" component={StartPage} />
+		<Route path="/photo-booth" component={PhotoBooth} />
+	</Router>
 ), document.getElementById('app'));
