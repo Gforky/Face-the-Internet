c79e43f72f022d96ffb519170ce7462366632f85
John Pett <email@johnpett.com>
2016-02-06 17:32:45 +0100

    Full width canvas.

--- app/components/Face.js
+++ app/components/Face.js
@@ -1,9 +1,16 @@
 /** @jsx React.DOM */
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
+    JQuery = require('jquery');
 var Face = React.createClass({
+	_createFabric: function() {
+		this.canvas = canvas = new fabric.Canvas(ReactDOM.findDOMNode(this.canvas));
+		canvas.setHeight(window.innerHeight);
+	    canvas.setWidth(window.innerWidth);
+	    canvas.renderAll();
+	},
 	componentDidMount: function() {
-		this.canvas = new fabric.Canvas(ReactDOM.findDOMNode(this.canvas));
+		this._createFabric();
 		fabric.Image.fromURL('img.jpg', function(img) {
 		  this.canvas.add(img);
 		}.bind(this));
